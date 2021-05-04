d4d3e3d877983ead56d91471786bf7b30bcdceb4
Aleix Suau <info@macrofonoestudio.es>
2021-05-04 11:16:11 +0200

    Work inside OnPush components

--- frontend/src/app/modules/common/dynamic-forms/components/dynamic-form/dynamic-form.component.ts
+++ frontend/src/app/modules/common/dynamic-forms/components/dynamic-form/dynamic-form.component.ts
@@ -7,6 +7,7 @@ import {
   EventEmitter,
   forwardRef,
   SimpleChanges,
+  ChangeDetectorRef,
 } from "@angular/core";
 import { FormlyForm } from "@ngx-formly/core";
 import { DynamicFormService } from "../../services/dynamic-form/dynamic-form.service";
@@ -154,6 +155,7 @@ export class DynamicFormComponent extends UntilDestroyedMixin implements Control
     private _pathHelperService:PathHelperService,
     private _notificationsService:NotificationsService,
     private _formsService: FormsService,
+    private _changeDetectorRef: ChangeDetectorRef,
   ) {
     super();
   }
@@ -293,6 +295,8 @@ export class DynamicFormComponent extends UntilDestroyedMixin implements Control
     this.form = form;
     this.fields = this.fieldsSettingsPipe ? this.fieldsSettingsPipe(fields) : fields;
     this.innerModel = model;
+
+    this._changeDetectorRef.detectChanges();
 
     if (!this.isStandaloneForm) {
       this.onChange(this.innerModel);
